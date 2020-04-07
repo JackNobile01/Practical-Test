78fd2302ee2539242a80facb5dd65ef6ab43cc15
JackNobile01 <63307344+JackNobile01@users.noreply.github.com>
2020-04-07 23:04:30 +0100

    First commit

--- newsapp/views.py
+++ newsapp/views.py
@@ -11,8 +11,5 @@ def detail(request, article_id):
 
 def index(request):
     latest_article_list = Article.objects.order_by('-pub_date_text')[:5]
-    template = loader.get_template('newsapp/index.html')
-    context = {
-        'latest_article_list': latest_article_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_article_list': latest_article_list}
+    return render(request, 'newsapp/index.html', context)

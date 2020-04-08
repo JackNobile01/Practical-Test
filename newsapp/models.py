229ab867e10969a44eba104f6e9d83bac99c9a07
JackNobile01 <63307344+JackNobile01@users.noreply.github.com>
2020-04-08 11:41:56 +0100

    Added in detail.html and made changes to index.html to add some design to page

--- newsapp/models.py
+++ newsapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-    heading_text = models.CharField(max_length=50)
+    heading_text = models.CharField(max_length=100)
     pub_date_text = models.DateTimeField('Date Published:')
     body_text = models.CharField(max_length=400)
